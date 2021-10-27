bb2edbd53c8df6575077a677aa62a303ebdc91b7
tailorv <maxwellmuthomijr@gmail.com>
2021-10-27 14:08:16 +0300

    update views.py

--- emp-api/api/views.py
+++ emp-api/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from django.http.response import JsonResponse
-from EmployeeApp.models import Departments,Employees
-from EmployeeApp.serializers import DepartmentSerializer,EmployeeSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from api.models import Departments,Employees
+from api.serializers import DepartmentSerializer,EmployeeSerializer
 from django.core.files.storage import default_storage
 
 # Create your views here.
@@ -14,29 +14,28 @@ def departmentsApi(request,id=0):
     if request.method=='GET':
         departments = Departments.objects.all()
         departments_serializer = DepartmentSerializer(departments, many=True)
-        return JsonResponse(departments_serializer.data, safe=False)
+        return Response(departments_serializer.data, safe=False)
 
     elif request.method=='POST':
-        department_data=JSONParser().parse(request)
-        department_serializer = DepartmentSerializer(data=department_data)
+        department_serializer = DepartmentSerializer(data=request.department_data)
         if department_serializer.is_valid():
             department_serializer.save()
-            return JsonResponse("Added Successfully!!" , safe=False)
-        return JsonResponse("Failed to Add.",safe=False)
+            return Response("Added Successfully!!" , safe=False)
+        return Response(department_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
     
+       
     elif request.method=='PUT':
-        department_data = JSONParser().parse(request)
-        department=Departments.objects.get(DepartmentId=department_data['DepartmentId'])
-        department_serializer=DepartmentSerializer(department,data=department_data)
+        department=Departments.objects.get(DepartmentId=request.department_data['DepartmentId'])
+        department_serializer=DepartmentSerializer(department,data=request.department_data)
         if department_serializer.is_valid():
             department_serializer.save()
-            return JsonResponse("Updated Successfully!!", safe=False)
-        return JsonResponse("Failed to Update.", safe=False)
-
+            return Response("Updated Successfully!!", safe=False)
+        return Response(department_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     elif request.method=='DELETE':
         department=Departments.objects.get(DepartmentId=id)
         department.delete()
-        return JsonResponse("Deleted Succeffully!!", safe=False)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST' , 'PUT','DELETE'])
 
@@ -44,34 +43,31 @@ def employeeApi(request,id=0):
     if request.method=='GET':
         employees = Employees.objects.all()
         employees_serializer = EmployeeSerializer(employees, many=True)
-        return JsonResponse(employees_serializer.data, safe=False)
+        return Response(employees_serializer.data, safe=False)
 
     elif request.method=='POST':
-        employee_data=JSONParser().parse(request)
-        employee_serializer = EmployeeSerializer(data=employee_data)
+        employee_serializer = EmployeeSerializer(data=request.employee_data)
         if employee_serializer.is_valid():
             employee_serializer.save()
-            return JsonResponse("Added Successfully!!" , safe=False)
-        return JsonResponse("Failed to Add.",safe=False)
-    
+            return Response(employee_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(employee_serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+     
     elif request.method=='PUT':
-        employee_data = JSONParser().parse(request)
-        employee=Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
-        employee_serializer=EmployeeSerializer(employee,data=employee_data)
+        employee=Employees.objects.get(EmployeeId=request.employee_data['EmployeeId'])
+        employee_serializer=EmployeeSerializer(employee,data=request.employee_data)
         if employee_serializer.is_valid():
             employee_serializer.save()
-            return JsonResponse("Updated Successfully!!", safe=False)
-        return JsonResponse("Failed to Update.", safe=False)
-
+            return Response("Updated Successfully!!", safe=False)
+        return Response(employee_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     elif request.method=='DELETE':
         employee=Employees.objects.get(EmployeeId=id)
         employee.delete()
-        return JsonResponse("Deleted Succeffully!!", safe=False)
-
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['SAVE'])
 def SaveFile(request):
     file=request.FILES['uploadedFile']
     file_name = default_storage.save(file.name,file)
 
-    return JsonResponse(file_name,safe=False)
+    return Response(file_name,safe=False)
